8f5f8b0ffa49514f67dd0cbd23fec501a8844679
sabarish1604 <sabarishravichandran1604@gmail.com>
2026-02-11 10:15:02 +0530

    Create FinallyDemo.java

---
+++ FinallyDemo.java
@@ -0,0 +1,11 @@
+public class FinallyDemo {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Exception caught");
+        } finally {
+            System.out.println("Finally block executed");
+        }
+    }
+}
